b859d252d3b7c34f6331aa0c27f0a511b9ce9e54
Hassan Chughtai <hchughta@uncc.edu>
2022-01-25 22:11:07 -0500

    Addtional exercise 1

---
+++ more_exercise_1.py
@@ -0,0 +1,8 @@
+# Group Members: Ivory Steed, Paul Thottappilly, Hassan Chughtai, Tony Le, Shekar Balasubramaniam
+
+def rev_func(x):
+  return x[::-1]
+
+text = rev_func(input("Enter a string: "))
+
+print(text)
